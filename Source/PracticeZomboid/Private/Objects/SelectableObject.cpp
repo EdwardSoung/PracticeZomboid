ec7362049a4c91451f1ac79739c15f58615dcd72
EdwardSoung <allegoria88@gmail.com>
2025-11-12 18:02:03 +0900

    SelectableObject 클래스 위치 수정

--- Source/PracticeZomboid/Private/Objects/SelectableObject.cpp
+++ Source/PracticeZomboid/Private/Objects/SelectableObject.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "SelectableObject.h"
+#include "Objects/SelectableObject.h"
 
 // Sets default values
 ASelectableObject::ASelectableObject()
